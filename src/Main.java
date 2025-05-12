4aab952022ff34de2d66736efa80451eb6ed0072
Saziso Buthelezi <sazisoshenge1@gmail.com>
2025-05-12 21:15:57 +0200

    finished up the code for the entry point like initializing the list and dislaying it

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+            StudentList studentList = new StudentList();
+            studentList.Initialize();
 
+        System.out.println("Displaying Students");
+
+        for (Student student : studentList.getStudents()){
+            System.out.println(student);
+        }
+
+        System.out.println("-----------------------------------------------------------");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many students you want to add? ");
+        int numberOfStudent = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i <= numberOfStudent; i++) {
+            addStudent add = new addStudent();
+            add.addStudentPrompt();
+        }
+
+        System.out.println("---------------------------------------------------------------");
+        for (Student student : studentList.getStudents()){
+            student.study();
+        }
     }
 }
--- src/addStudent.java
+++ src/addStudent.java
@@ -3,12 +3,32 @@ import java.util.Scanner;
 
 public class addStudent {
 
-    ArrayList<Student> students = new ArrayList<>();
+
     StudentList studentList = new StudentList();
     Scanner sc = new Scanner(System.in);
 
 
     public void addStudentPrompt(){
+        System.out.println("Please enter student Number: ");
+        String stdNumber = sc.nextLine();
+
+        System.out.println("Please enter student Name: ");
+        String stdName = sc.nextLine();
+
+        System.out.println("Please enter student surname: ");
+        String stdSurname = sc.nextLine();
+
+        System.out.println("Please enter student course: ");
+        String stdCourse = sc.nextLine();
+
+        if(stdCourse.equalsIgnoreCase("Degree")){
+            studentList.addStudent(new DegreeStudent(stdNumber, stdName, stdSurname, stdCourse));
+        } else if (stdCourse.equalsIgnoreCase("Diploma")) {
+            studentList.addStudent(new DiplomaStudent(stdNumber, stdName, stdSurname, stdCourse));
+        }else {
+            System.out.println("Invalid course type, student not added");
+        }
+
 
     }
 
