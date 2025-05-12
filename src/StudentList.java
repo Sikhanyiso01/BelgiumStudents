5d8dd78bc6a59266f335c81b275a7f13876add7f
Saziso Buthelezi <sazisoshenge1@gmail.com>
2025-05-12 20:54:04 +0200

    added an addStudent class

--- src/StudentList.java
+++ src/StudentList.java
@@ -1,9 +1,21 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class StudentList {
     static ArrayList students = new ArrayList();
 
     public void Initialize(){
         students.add(new DiplomaStudent("601089", "Saziso", "Buthelezi", "Diploma" ));
+        students.add(new DiplomaStudent("601078", "Sizwe", "Viviers", "Diploma" ));
+        students.add(new DiplomaStudent("678977", "Vince", "Collins", "Degree" ));
+        students.add(new DiplomaStudent("654567", "Tom", "Nhlanzi", "Degree" ));
+    }
+
+    public static void addStudent(Student student){
+        students.add(student);
+    }
+
+    public List<Student> getStudents(){
+        return students;
     }
 }
---
+++ src/addStudent.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class addStudent {
+
+    ArrayList<Student> students = new ArrayList<>();
+    StudentList studentList = new StudentList();
+    Scanner sc = new Scanner(System.in);
+
+
+    public void addStudentPrompt(){
+
+    }
+
+}
